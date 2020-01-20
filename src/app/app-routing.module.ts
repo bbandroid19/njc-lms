317073bbd59d9182756297ae38341099789a342e
Basil Baby <basilbaby@Basils-MacBook-Air.local>
2020-01-20 12:02:14 +0530

    Assets added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from './components/layout/home/home.component';
+import {FormsComponent} from './components/layout/forms/forms.component';
+import {RegisterComponent} from './components/layout/register/register.component';
+import {LoginComponent} from './components/layout/login/login.component';
+import {CoursesComponent} from './components/sites/courses/courses.component';
+import {MembershipComponent} from './components/sites/membership/membership.component';
+import { from } from 'rxjs';
+import { AboutComponent } from './components/sites/about/about.component';
+import { FaqComponent } from './components/sites/faq/faq.component';
+import { ErrorComponent } from './components/layout/error/error.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{ path: '', component: HomeComponent},
+{ path: 'login', component: LoginComponent },
+{ path: 'courses', component: CoursesComponent },
+{ path: 'membership', component: MembershipComponent },
+{ path: 'about-us', component: AboutComponent },
+{ path: 'faq', component: FaqComponent},
+{ path: 'not-found', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { SidemenuComponent } from './components/layout/sidemenu/sidemenu.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/layout/home/home.component';
+import { FormsComponent } from './components/layout/forms/forms.component';
+import { LoginComponent } from './components/layout/login/login.component';
+import { RegisterComponent } from './components/layout/register/register.component';
+import { BottomfooterComponent } from './components/layout/bottomfooter/bottomfooter.component';
+import { CoursesComponent } from './components/sites/courses/courses.component';
+import { MembershipComponent } from './components/sites/membership/membership.component';
+import { AboutComponent } from './components/sites/about/about.component';
+import { FaqComponent } from './components/sites/faq/faq.component';
+import { ErrorComponent } from './components/layout/error/error.component';
+import { ContactComponent } from './components/sites/contact/contact.component';
+import { SubscribeComponent } from './components/layout/subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidemenuComponent,
+    FooterComponent,
+    HomeComponent,
+    FormsComponent,
+    LoginComponent,
+    RegisterComponent,
+    BottomfooterComponent,
+    CoursesComponent,
+    MembershipComponent,
+    AboutComponent,
+    FaqComponent,
+    ErrorComponent,
+    ContactComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
